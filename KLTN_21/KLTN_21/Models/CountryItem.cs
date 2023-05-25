a051b6cf238c368b9bdfb882ba47e412d56c5ece
imHughie <94167057+imHughie@users.noreply.github.com>
2023-05-25 09:26:58 +0700

    first commit

---
+++ KLTN_21/KLTN_21/Models/CountryItem.cs
@@ -0,0 +1,11 @@
+﻿namespace KLTN_21.Models
+{
+    public class CountryItem
+    {
+
+        public long Id { get; set; }
+        public string? Name { get; set; }
+
+        public bool IsDone { get; set; } 
+    }
+}
